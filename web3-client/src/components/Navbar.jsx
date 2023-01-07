27101fe05a7758e06d86382098af47716e57bdf1
Shrikant Paliwal <shrikantpaliwal001@gmail.com>
2023-01-08 01:02:12 +0530

    Navbar checkin

--- web3-client/src/components/Navbar.jsx
+++ web3-client/src/components/Navbar.jsx
@@ -1,5 +1,7 @@
+import React from "react";
 import { HiMenuAlt4 } from 'react-icons/hi';
 import { AiOutlineClose } from 'react-icons/ai';
+
 
 import logo from '../../images/logo.png';
 
@@ -12,6 +14,8 @@ const NavbarItem = ({ title, classProps }) => {
 }
 
 const Navbar = () => {
+    const [toggleMenu, setToggleMenu] = React.useState(false);
+
     return (
         <nav className="w-full flex md:justify-center justify-between items-center p-4">
             <div className="md:flex-[0.5] flex-initial justify-center items-center">
@@ -24,6 +28,23 @@ const Navbar = () => {
                 }
                 <li className="bg-[#2952e3] py-2 px-7 mx-5 rounded-full cursor-pointer hover:bg-[#2546bd]">Login</li>
             </ul>
+            <div className="flex relative">
+                {toggleMenu ?
+                    <AiOutlineClose fontSize={24} className="text-white md:hidden cursor-pointer" onClick={() => (setToggleMenu(false))} /> :
+                    <HiMenuAlt4 fontSize={24} className="text-white md:hidden cursor-pointer" onClick={() => (setToggleMenu(true))} />
+                }
+                {toggleMenu && (
+                    <ul className="z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
+                    flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in">
+                        <li className="text-xl w-full py-2"><AiOutlineClose onClick={() => (setToggleMenu(false))} /></li>
+                        {["Market", "Exchange", "Tutorials", "Wallets"].map((items, index) => (
+                            <NavbarItem key={items + index} title={items} classProps="py-2 text-lg" />
+                        ))
+                        }
+                    </ul>
+                )
+                }
+            </div>
         </nav>
     );
 }
